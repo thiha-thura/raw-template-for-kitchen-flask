ef84517f072ca949d7c498532581103b36a35622
Thiha Thura <88715513+thiha-thura@users.noreply.github.com>
2025-05-12 22:17:53 +0630

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@localhost/menu"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root@localhost/menu"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 db =   SQLAlchemy(app)
